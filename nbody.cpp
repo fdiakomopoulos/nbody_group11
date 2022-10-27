0d14a1c149559c5fd241623d59a75450011d3444
Charlesckw <Charlesckw@users.noreply.github.com>
2022-10-27 16:40:10 +0200

    description

--- nbody.cpp
+++ nbody.cpp
@@ -16,6 +16,8 @@
 #define _USE_MATH_DEFINES // https://docs.microsoft.com/en-us/cpp/c-runtime-library/math-constants?view=msvc-160
 #include <cmath>
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 
 // these values are constant and not allowed to be changed
