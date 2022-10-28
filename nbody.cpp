eefb2ef1b71b7b88517da5092f8f1a6dca884d6a
Faidon Diak <fdiakomopoulos@noreply.github.com>
2022-10-28 14:59:36 +0200

    Parameters

--- nbody.cpp
+++ nbody.cpp
@@ -299,7 +299,7 @@ int main(int argc, char **argv) {
         clock_t total_time=end_time-start_time;
         double time=(total_time)/(double) CLOCKS_PER_SEC;
         std::cout << "the total time of the cpp program is: "<< time << "sec" << std::endl;
-        std::cout << "-------------"
+        std::cout << "-------------";
         return EXIT_SUCCESS;
     }
 }
